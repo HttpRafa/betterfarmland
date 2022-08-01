c1bb577899bca69afbcb7254592d4f2f122c199c
HttpRafa <rafael.kienitz@gmail.com>
2022-08-01 21:33:20 +0200

    Added fabric mod

---
+++ fabric/remappedSrc/net/fabricmc/example/ExampleMod.java
@@ -0,0 +1,15 @@
+package net.fabricmc.example;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ExampleMod implements ModInitializer {
+
+	public static final Logger LOGGER = LoggerFactory.getLogger("betterfarmland");
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ fabric/src/main/java/de/rafael/mods/better/farmland/mixin/FarmlandBlockMixin.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2022. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright notice,
+ *         this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *         notice, this list of conditions and the following disclaimer in the
+ *         documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the developer nor the names of its contributors
+ *         may be used to endorse or promote products derived from this software
+ *         without specific prior written permission.
+ *     * Redistributions in source or binary form must keep the original package
+ *         and class name.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package de.rafael.mods.better.farmland.mixin;
+
+//------------------------------
+//
+// This class was developed by Rafael K.
+// On 08/01/2022 at 8:44 PM
+// In the project BetterFarmland
+//
+//------------------------------
+
+import de.rafael.mods.better.farmland.BetterFarmland;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.FarmlandBlock;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(FarmlandBlock.class)
+public abstract class FarmlandBlockMixin {
+
+    @Inject(method = "onLandedUpon", at = @At("HEAD"), cancellable = true)
+    public void onLandedUpon(World world, BlockState state, BlockPos pos, Entity entity, float fallDistance, CallbackInfo ci) {
+        //ci.cancel();
+    }
+
+
+}
