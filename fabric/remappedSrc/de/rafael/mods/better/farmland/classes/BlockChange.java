f58ec6e13118da2da8ad8d4ebe20342240461352
HttpRafa <rafael.kienitz@gmail.com>
2022-08-05 23:57:38 +0200

    Updated to 1.19.2

---
+++ fabric/remappedSrc/de/rafael/mods/better/farmland/classes/BlockChange.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2022. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright notice,
+ *         this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *         notice, this list of conditions and the following disclaimer in the
+ *         documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the developer nor the names of its contributors
+ *         may be used to endorse or promote products derived from this software
+ *         without specific prior written permission.
+ *     * Redistributions in source or binary form must keep the original package
+ *         and class name.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package de.rafael.mods.better.farmland.classes;
+
+//------------------------------
+//
+// This class was developed by Rafael K.
+// On 07/16/2022 at 2:55 PM
+// In the project BetterFarmland
+//
+//------------------------------
+
+import net.minecraft.item.Item;
+import net.minecraft.sound.SoundEvent;
+
+public record BlockChange(ChangeSound sound, Item from, Item to, ChangeDrop drop, int newAge) {
+
+    @Override
+    public String toString() {
+        return "BlockChange{" +
+                "sound=" + sound +
+                ", from=" + from +
+                ", to=" + to +
+                ", drop=" + drop +
+                ", newAge=" + newAge +
+                '}';
+    }
+
+    public record ChangeSound(SoundEvent sound, float soundVolume, float soundPitch) {
+
+        @Override
+        public String toString() {
+            return "ChangeSound{" +
+                    "sound=" + sound +
+                    ", soundVolume=" + soundVolume +
+                    ", soundPitch=" + soundPitch +
+                    '}';
+        }
+
+    }
+
+    public record ChangeDrop(Item item, int amount) {
+
+        @Override
+        public String toString() {
+            return "ChangeDrop{" +
+                    "item=" + item +
+                    ", amount=" + amount +
+                    '}';
+        }
+
+    }
+
+}
